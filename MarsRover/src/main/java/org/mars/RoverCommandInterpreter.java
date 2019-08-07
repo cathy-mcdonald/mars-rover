10331618faf594db58d2dfe74794087cfef51d48
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2019-08-07 14:53:02 +1000

    Setup checkstyle

--- MarsRover/src/main/java/org/mars/RoverCommandInterpreter.java
+++ MarsRover/src/main/java/org/mars/RoverCommandInterpreter.java
@@ -2,9 +2,9 @@ package org.mars;
 
 public class RoverCommandInterpreter {
 
-	public static void main(String[] args) {
-		System.out.println("Mars Rover v1.0 closed.");
+  public static void main(String[] args) {
+    System.out.println("Mars Rover v1.0 closed.");
 
-	}
+  }
 
 }
