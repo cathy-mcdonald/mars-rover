696779f3f83c5a2a419409fde1ff8b7cc8d28983
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2019-08-07 18:09:04 +1000

    Add code to set up the plateau and start processing input

--- MarsRover/src/main/java/org/mars/RoverCommandInterpreter.java
+++ MarsRover/src/main/java/org/mars/RoverCommandInterpreter.java
@@ -1,10 +1,51 @@
 package org.mars;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class RoverCommandInterpreter {
+  
+  private static Plateau plateau;
 
   public static void main(String[] args) {
-    System.out.println("Mars Rover v1.0 closed.");
+    setPlateau("plateau.txt");
+    System.out.println("Mars Rover v1.0 running, plateau configuration is:");
+    processInput(new InputStreamReader(System.in));
+  }
+  
+  public static void setPlateau(String fileName) {
+    plateau = new Plateau(new File(fileName));
+  }
+  
+  public static void processInput(Reader reader) {
 
+    int data = 0;
+    char dataChar = '\n';
+    
+    while ((data != -1) && (Character.toUpperCase(dataChar) != 'X')) {
+      if (dataChar == '\n') {
+        System.out.println(plateau.mapWithRover(0, 5));
+        System.out.println("Waiting for commands.");
+        System.out.print(">");
+      } else {
+        processCommand(dataChar);
+      }
+      try {
+        data = reader.read();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      dataChar = (char) data;
+    }
+ 
+    System.out.println("Mars Rover v1.0 closed.");
+    
+  }
+  
+  public static void processCommand(char command) {
+    //TODO Make the rover do stuff
   }
 
 }
--- MarsRover/src/test/java/org/mars/CommandLineTests.java
+++ MarsRover/src/test/java/org/mars/CommandLineTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.StringReader;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.Test;
 
 public class CommandLineTests {
   private static final String EOL = System.getProperty("line.separator");
+  private static final String WAITING = "Waiting for commands." + EOL + ">";
   private static final String ROVER_CLOSED = "Mars Rover v1.0 closed." + EOL;
   private PrintStream console;
   private ByteArrayOutputStream bytes;
@@ -20,6 +22,7 @@ public class CommandLineTests {
     bytes = new ByteArrayOutputStream();
     console = System.out;
     System.setOut(new PrintStream(bytes));
+    RoverCommandInterpreter.setPlateau("src/test/resources/plateau.txt");
   }
 
   @After
@@ -28,10 +31,37 @@ public class CommandLineTests {
   }
 
   @Test
-  public void test() {
-    String[] args = new String[0];
-    RoverCommandInterpreter.main(args);
-    assertEquals(ROVER_CLOSED, bytes.toString());
+  public void testDoNothing() {
+    String inputString = "x";
+    StringReader input = new StringReader(inputString);
+    RoverCommandInterpreter.processInput(input);
+    String expectedPlateau = "XoooooRRRR\n"
+        + "ooRooooooo\n"
+        + "ooooooRRoo\n"
+        + "ooRooooooo\n"
+        + "oooooRoooo\n"
+        + "oooooRRRoo\n\n";
+    assertEquals(expectedPlateau + WAITING + ROVER_CLOSED, bytes.toString());
+  }
+  
+  @Test
+  public void testMoveOnce() {
+    String inputString = "M\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedPlateau = "XoooooRRRR\n"
+        + "ooRooooooo\n"
+        + "ooooooRRoo\n"
+        + "ooRooooooo\n"
+        + "oooooRoooo\n"
+        + "oooooRRRoo\n\n";
+    String expectedNextPlateau = "oXooooRRRR\n"
+        + "ooRooooooo\n"
+        + "ooooooRRoo\n"
+        + "ooRooooooo\n"
+        + "oooooRoooo\n"
+        + "oooooRRRoo\n\n";
+    RoverCommandInterpreter.processInput(input);
+    assertEquals(expectedPlateau + WAITING + expectedNextPlateau + WAITING + ROVER_CLOSED, bytes.toString());
   }
 
 }
