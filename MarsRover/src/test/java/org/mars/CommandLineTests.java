0eb9f4cc67f969cab1c686755a3832f032e8fa0e
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2019-08-08 10:06:19 +1000

    Flesh out unit tests

--- MarsRover/src/test/java/org/mars/CommandLineTests.java
+++ MarsRover/src/test/java/org/mars/CommandLineTests.java
@@ -12,17 +12,25 @@ import org.junit.Test;
 
 public class CommandLineTests {
   private static final String EOL = System.getProperty("line.separator");
+  private static final String ROVER_RUNNING = "Mars Rover v1.0 running, plateau configuration is:" + EOL + EOL;
   private static final String WAITING = "Waiting for commands." + EOL + ">";
-  private static final String ROVER_CLOSED = "Mars Rover v1.0 closed." + EOL;
+  private static final String ROVER_CLOSED = EOL + "Mars Rover v1.0 closed." + EOL;
   private PrintStream console;
-  private ByteArrayOutputStream bytes;
+  private ByteArrayOutputStream outputBytes;
+  private String expectedPlateau;
 
   @Before
   public void setUp() throws Exception {
-    bytes = new ByteArrayOutputStream();
+    outputBytes = new ByteArrayOutputStream();
     console = System.out;
-    System.setOut(new PrintStream(bytes));
+    System.setOut(new PrintStream(outputBytes));
     RoverCommandInterpreter.setPlateau("src/test/resources/plateau.txt");
+    expectedPlateau = "XoooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
   }
 
   @After
@@ -34,34 +42,287 @@ public class CommandLineTests {
   public void testDoNothing() {
     String inputString = "x";
     StringReader input = new StringReader(inputString);
+    String expectedResult = EOL + "Sent 0 command(s) / 0 failed." + EOL;
+    
     RoverCommandInterpreter.processInput(input);
-    String expectedPlateau = "XoooooRRRR\n"
-        + "ooRooooooo\n"
-        + "ooooooRRoo\n"
-        + "ooRooooooo\n"
-        + "oooooRoooo\n"
-        + "oooooRRRoo\n\n";
-    assertEquals(expectedPlateau + WAITING + ROVER_CLOSED, bytes.toString());
+    
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
   }
   
   @Test
   public void testMoveOnce() {
     String inputString = "M\nX";
     StringReader input = new StringReader(inputString);
-    String expectedPlateau = "XoooooRRRR\n"
-        + "ooRooooooo\n"
-        + "ooooooRRoo\n"
-        + "ooRooooooo\n"
-        + "oooooRoooo\n"
-        + "oooooRRRoo\n\n";
-    String expectedNextPlateau = "oXooooRRRR\n"
-        + "ooRooooooo\n"
-        + "ooooooRRoo\n"
-        + "ooRooooooo\n"
-        + "oooooRoooo\n"
-        + "oooooRRRoo\n\n";
+    String expectedNextPlateau = "oXooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = EOL + "Sent 1 command(s) / 0 failed." + EOL;
+    
     RoverCommandInterpreter.processInput(input);
-    assertEquals(expectedPlateau + WAITING + expectedNextPlateau + WAITING + ROVER_CLOSED, bytes.toString());
+
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
   }
 
+  @Test
+  public void testMultipleCommands() {
+    String inputString = "M\nM\nM\nR\nM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedPlateau2 = "oXooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedPlateau3 = "ooXoooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedPlateau4 = "oooXooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedPlateau5 = "ooooooRRRR" + EOL
+        + "ooRXoooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 5 command(s) / 0 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau2);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau3);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau4);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau5);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+
+  @Test
+  public void testMultipleCommandsOneLine() {
+    String inputString = "MMMRM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "ooooooRRRR" + EOL
+        + "ooRXoooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 5 command(s) / 0 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+  
+  @Test
+  public void testLeftTurn() {
+    String inputString = "MRMMLM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "ooooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooXoooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 6 command(s) / 0 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+  
+  @Test
+  public void testTooFarNorthFailure() {
+    String inputString = "LM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "XoooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 2 command(s) / 1 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+
+  @Test
+  public void testTooFarSouthFailure() {
+    String inputString = "RMMMMMM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "ooooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "XooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 7 command(s) / 1 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+  
+  @Test
+  public void testTooFarWestFailure() {
+    String inputString = "RRM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "XoooooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 3 command(s) / 1 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+  
+  @Test
+  public void testTooFarEastFailure() {
+    String inputString = "MMMMMRMMMMM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "XoooooRRRR" + EOL
+        + "ooRooooooX" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 11 command(s) / 1 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+  
+  @Test
+  public void testRockFailure() {
+    String inputString = "MMRM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "ooXoooRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 4 command(s) / 1 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+
+  @Test
+  public void testThreeFailures() {
+    String inputString = "LMRMMRMLMMMM\nX";
+    StringReader input = new StringReader(inputString);
+    String expectedNextPlateau = "oooooXRRRR" + EOL
+        + "ooRooooooo" + EOL
+        + "ooooooRRoo" + EOL
+        + "ooRooooooo" + EOL
+        + "oooooRoooo" + EOL
+        + "oooooRRRoo" + EOL + EOL;
+    String expectedResult = "Sent 12 command(s) / 1 failed." + EOL;
+    
+    RoverCommandInterpreter.processInput(input);
+    int outputCursor = 0;
+    String outputString = outputBytes.toString();
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_RUNNING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedNextPlateau);
+    outputCursor = validatePartialOutput(outputString, outputCursor, WAITING);
+    outputCursor = validatePartialOutput(outputString, outputCursor, expectedResult);
+    outputCursor = validatePartialOutput(outputString, outputCursor, ROVER_CLOSED);
+  }
+
+  private int validatePartialOutput(String outputString, int outputCursor, String expectedString) {
+    assertEquals(expectedString, outputString.substring(outputCursor, outputCursor + expectedString.length()));
+    return outputCursor + expectedString.length();
+  }
 }
